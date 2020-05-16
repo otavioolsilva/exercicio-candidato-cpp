134433e1f37519548f6ddad1fc24f6760bbea8f4
Otavio Silva <otavioolsilva@gmail.com>
2020-05-16 17:43:35 -0300

    Leitura da escolaridade

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int escolaridade;
+
+    //Leitura da escolaridade
+    cout << "Qual sua escolaridade?" << endl;
+    cout << "1) Ensino Fundamental" << endl;
+    cout << "2) Ensino Medio" << endl;
+    cout << "3) Ensino Superior" << endl;
+    cout << "4) Pos-graduacao" << endl;
+    cout << "Digite uma opcao: ";
+    cin >> escolaridade;
+
     return 0;
 }
