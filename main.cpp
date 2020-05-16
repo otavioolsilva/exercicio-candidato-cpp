44ba8a87ca72ddf6a7cc0d68c4ddd656cada1973
Otavio Silva <otavioolsilva@gmail.com>
2020-05-16 18:15:28 -0300

    Resultado final

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main()
 {
     int escolaridade, experiencia;
     string viajar, habilitacao;
+    bool cAssistente = 0, cGerente = 0, cAnalista = 0;
 
     //Leitura da escolaridade
     cout << "Qual sua escolaridade?" << endl;
@@ -31,6 +32,20 @@ int main()
     else if(experiencia <= 5) experiencia = 20;
     else experiencia = 40;
     cout << "Pontos por experiencia: " << experiencia << endl;
+
+    //Resultado
+    if(escolaridade >= 2 && habilitacao == "S") cAssistente = 1;
+    if(escolaridade >= 3 && experiencia >= 20) cGerente = 1;
+    if(escolaridade >= 3 && experiencia >= 40 && viajar == "S") cAnalista = 1;
+
+    if(cAssistente == 0 && cGerente == 0 && cAnalista == 0){
+        cout << endl << "Infelizmente seu perfil nao atende a empresa" << endl;
+    } else{
+        cout << endl << "Voce esta habilitado para o(s) seguinte(s) cargo(s):" << endl;
+        if(cAssistente == 1) cout << "ASSISTENTE" << endl;
+        if(cGerente == 1) cout << "GERENTE" << endl;
+        if (cAnalista == 1) cout << "ANALISTA" << endl;
+    }
 
     return 0;
 }
